940ecc43b20f2acb2c880a60afaa292c9c5d6df2
MatBro <uzytkownik@kursgit.pl>
2023-03-19 13:13:15 +0100

    fix1

--- quickbook/cal/admin.py
+++ quickbook/cal/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Event
 
+
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'day', 'start_time', 'end_time', 'salon', 'employee']
--- quickbook/cal/views.py
+++ quickbook/cal/views.py
@@ -17,6 +17,7 @@ from users.models import Employee
 def index(request):
     return HttpResponse('hello')
 
+
 class CalendarView(ListView):
     model = Event
     template_name = 'cal/calendar.html'
@@ -32,11 +33,13 @@ class CalendarView(ListView):
         context['next_month'] = next_month(d)
         return context
 
+
 def prev_month(d):
     first = d.replace(day=1)
     prev_month = first - timedelta(days=1)
     month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
     return month
+
 
 def next_month(d):
     days_in_month = calendar.monthrange(d.year, d.month)[1]
@@ -44,6 +47,7 @@ def next_month(d):
     next_month = last + timedelta(days=1)
     month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
     return month
+
 
 def get_date(req_day):
     if req_day:
