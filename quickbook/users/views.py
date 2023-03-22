f6bfca17d36f7b90d2032f0864016be3c00f3e7a
Mateusz Brożyna <123745307+tivolast@users.noreply.github.com>
2023-03-22 16:21:14 +0100

    Views fixing (#12)
    * Adding Employee List
    
    * Adding List of Employees to each salon
    
    * Adding confirm views to edit users
    
    * empty line in employee_confirm_delete
    
    * poprawiona literówka
    
    * literówka
    
    ---------
    
    Co-authored-by: MatBro <uzytkownik@kursgit.pl>
    Co-authored-by: House079 <98473955+House079@users.noreply.github.com>

--- quickbook/users/views.py
+++ quickbook/users/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, ListView, DeleteView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import UpgradedUserCreationForm, CustomAuthenticationForm
 from django.contrib.auth.views import LoginView
@@ -24,4 +24,23 @@ class CustomLoginView(LoginView):
     template_name = 'registration/login.html'
     authentication_form = CustomAuthenticationForm
     success_url = reverse_lazy('/')
+
+
+class EmployeesList(ListView):
+    model = Employee
+    template_name = 'users/employees.html'
+    fields = ('name', 'surname', 'email', 'date_joined')
+
+
+class EmployeeDelete(DeleteView):
+    model = Employee
+    success_url = reverse_lazy('users:employees_list')
+
+
+class EmployeeUpdate(UpdateView):
+    model = Employee
+    template_name = 'users/employee_update_form.html'
+    fields = ('username', 'name', 'surname', 'email', 'salon')
+    success_url = reverse_lazy('users:employees_list')
+
 
