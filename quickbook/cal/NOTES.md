078aeec515d0c44ba013c968f4943c9362fe06f6
Janusz Skotarczak <skotarczak.janusz@gmail.com>
2023-03-23 22:21:38 +0100

    Memory of calendar navigation without remembering the salon and employee.

--- quickbook/cal/NOTES.md
+++
@@ -1,2 +0,0 @@
-## JavaScrip to ustawiania parametrów w URI
-https://stackoverflow.com/questions/486896/adding-a-parameter-to-the-url-with-javascript
--- quickbook/cal/views.py
+++ quickbook/cal/views.py
@@ -53,7 +53,7 @@ class EventDelete(DeleteView):
 def prev_month(d):
     first = d.replace(day=1)
     prev_month = first - timedelta(days=1)
-    month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    month = str(prev_month.year) + '-' + str(prev_month.month)
     return month
 
 
@@ -61,7 +61,7 @@ def next_month(d):
     days_in_month = calendar.monthrange(d.year, d.month)[1]
     last = d.replace(day=days_in_month)
     next_month = last + timedelta(days=1)
-    month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    month = str(next_month.year) + '-' + str(next_month.month)
     return month
 
 
