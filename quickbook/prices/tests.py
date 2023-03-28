6d3a8a166b1fa0cbb106b1f945ce89c898b98e5f
MatBro <uzytkownik@kursgit.pl>
2023-03-28 21:40:01 +0200

    added tests for models

--- quickbook/prices/tests.py
+++ quickbook/prices/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from django.utils import timezone
+from datetime import timedelta
+from .models import EventType
 
-# Create your tests here.
+
+class EventTypeModelTestCase(TestCase):
+    def setUp(self):
+        self.event_type = EventType.objects.create(
+            event_name='Test event',
+            price=100.00,
+            duration=timedelta(hours=1)
+        )
+
+    def test_event_type_creation(self):
+        self.assertTrue(isinstance(self.event_type, EventType))
+        self.assertEqual(self.event_type.__str__(), f'{self.event_type.event_name} | {self.event_type.duration}')
+
+    def test_event_type_price(self):
+        self.assertEqual(self.event_type.price, 100.00)
+
+    def test_event_type_duration(self):
+        self.assertEqual(self.event_type.duration, timedelta(hours=1))
+
+    def test_event_type_default_duration(self):
+        event_type = EventType.objects.create(
+            event_name='Test event without duration',
+            price=50.00
+        )
+        self.assertEqual(event_type.duration, timedelta(minutes=0, hours=0))
+
+    def test_event_type_update(self):
+        new_price = 150.00
+        self.event_type.price = new_price
+        self.event_type.save()
+        updated_event_type = EventType.objects.get(id=self.event_type.id)
+        self.assertEqual(updated_event_type.price, new_price)
+
+    def test_event_type_delete(self):
+        event_type_id = self.event_type.id
+        self.event_type.delete()
+        with self.assertRaises(EventType.DoesNotExist):
+            EventType.objects.get(id=event_type_id)
--- quickbook/salon/tests.py
+++ quickbook/salon/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from .models import Salon
 
-# Create your tests here.
+
+class SalonModelTest(TestCase):
+
+    def setUp(self):
+        Salon.objects.create(
+            name='Example Salon',
+            city='Łódź',
+            address='Piotrkowska 34',
+            details='This is an example salon.'
+        )
+
+    def test_salon_creation(self):
+        salon = Salon.objects.get(name='Example Salon')
+        self.assertEqual(salon.city, 'Łódź')
+        self.assertEqual(salon.address, 'Piotrkowska 34')
+        self.assertEqual(salon.details, 'This is an example salon.')
+
+    def test_salon_string_representation(self):
+        salon = Salon.objects.get(name='Example Salon')
+        self.assertEqual(str(salon), 'Example Salon')
+
+    def test_salon_name_max_length(self):
+        salon = Salon.objects.get(name='Example Salon')
+        max_length = salon._meta.get_field('name').max_length
+        self.assertEqual(max_length, 50)
+
+    def test_salon_city_max_length(self):
+        salon = Salon.objects.get(name='Example Salon')
+        max_length = salon._meta.get_field('city').max_length
+        self.assertEqual(max_length, 30)
+
+    def test_salon_address_max_length(self):
+        salon = Salon.objects.get(name='Example Salon')
+        max_length = salon._meta.get_field('address').max_length
+        self.assertEqual(max_length, 255)
